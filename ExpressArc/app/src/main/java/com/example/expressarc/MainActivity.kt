43bafb331649bacf272885c7e85b602a43606935
luc-ads <lucas.psilva@bandtec.com.br>
2023-07-18 20:28:01 -0300

    Lógica para exbir os números  por sorteio finalizada com sucesso

--- ExpressArc/app/src/main/java/com/example/expressarc/MainActivity.kt
+++ ExpressArc/app/src/main/java/com/example/expressarc/MainActivity.kt
@@ -2,7 +2,9 @@ package com.example.expressarc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.example.expressarc.databinding.ActivityMainBinding
+import java.util.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +15,38 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.button.setOnClickListener {
+            sorteio(binding.editNumber.text.toString())
+        }
+    }
 
+    private fun sorteio(valorDigitado: String) {
+
+        if (valorDigitado.isEmpty()) {
+            toastAlert("Insira um valor entre 6 e 15!")
+            return
+        }
+
+        val convertValue = valorDigitado.toInt()
+        if (convertValue < 6 || convertValue > 15) {
+            toastAlert("Insira um valor entre 6 e 15!")
+            return
+        }
+
+        val numbers = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val numberThis = random.nextInt(60)
+
+            numbers.add(numberThis + 1)
+            if (numbers.size == convertValue) break
+        }
+
+        binding.txtResult.text = numbers.joinToString(" - ")
+    }
+
+    private fun toastAlert(alertMessage: String) {
+        Toast.makeText(this, alertMessage, Toast.LENGTH_SHORT).show()
     }
 }

cdaaa15e688d34a189b2fff3e774f1c36e85b517
luc-ads <lucas.psilva@bandtec.com.br>
2023-07-19 20:48:49 -0300

    Add configuração para o viewBinding e criando a activity IMC

--- ExpressArc/app/src/main/java/com/example/expressarc/IMCActivity.kt
+++ ExpressArc/app/src/main/java/com/example/expressarc/IMCActivity.kt
@@ -2,15 +2,15 @@ package com.example.expressarc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.expressarc.databinding.ActivityMainBinding
+import com.example.expressarc.databinding.ActivityImcBinding
 
-class MainActivity : AppCompatActivity() {
+class IMCActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityImcBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityImcBinding.inflate(layoutInflater)
         setContentView(binding.root)
     }
 }

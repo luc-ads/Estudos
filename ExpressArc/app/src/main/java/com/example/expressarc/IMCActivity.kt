304a4f6308c62b6b2479f57e70bac0d09f4f7b92
luc-ads <lucas.psilva@bandtec.com.br>
2023-07-21 13:51:57 -0300

    Lógica de validação dos campos ao clicar no botão

--- ExpressArc/app/src/main/java/com/example/expressarc/IMCActivity.kt
+++ ExpressArc/app/src/main/java/com/example/expressarc/IMCActivity.kt
@@ -2,6 +2,7 @@ package com.example.expressarc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import androidx.core.content.ContextCompat
 import com.example.expressarc.databinding.ActivityImcBinding
 
@@ -14,6 +15,23 @@ class IMCActivity : AppCompatActivity() {
         binding = ActivityImcBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        initValues()
+    }
+
+    private fun initValues() {
+        with(binding) {
+            btnImcSend.setOnClickListener {
+                if (editImcWeight.text.toString().isEmpty() || editImcHeight.text.toString().isEmpty() || editImcWeight.text.toString().startsWith("0") || editImcHeight.text.toString().startsWith("0")) {
+                    Toast.makeText(this@IMCActivity, getString(R.string.message_error_edit_text), Toast.LENGTH_SHORT).show()
+                    return@setOnClickListener
+                }
+
+                calcImc(editImcWeight.text.toString().toInt(), editImcHeight.text.toString().toInt())
+            }
+        }
+    }
+
+    private fun calcImc(weight: Int, height: Int) {
 
     }
 }

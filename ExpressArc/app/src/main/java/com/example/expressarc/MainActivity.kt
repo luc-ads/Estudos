e044840f72d372e0b41f129ab250aaef0f44676b
luc-ads <lucas.psilva@bandtec.com.br>
2023-07-22 14:12:41 -0300

    Add dados mutáveis na recycler e mudando o layout manager para GridLayout

--- ExpressArc/app/src/main/java/com/example/expressarc/MainActivity.kt
+++ ExpressArc/app/src/main/java/com/example/expressarc/MainActivity.kt
@@ -1,16 +1,19 @@
 package com.example.expressarc
 
 import android.content.Intent
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.expressarc.databinding.ActivityMainBinding
+import com.example.expressarc.model.MainItem
 import com.example.expressarc.recycler.AdapterMain
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-    val listName = mutableListOf<String>()
+    val listName = mutableListOf<MainItem>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,11 +24,19 @@ class MainActivity : AppCompatActivity() {
 //            startActivity(Intent(this, IMCActivity::class.java))
 //        }
 
-        repeat(10) {
-            listName.add("Lucas Pereira da Silva $it")
+        repeat(20) {
+            listName.add(
+                MainItem(
+                    id = 1,
+                    drawableId = R.drawable.ic_launcher_background,
+                    textStringId = R.string.imc_response,
+                    color = Color.GREEN
+                ),
+            )
         }
 
-        binding.rvMain.adapter = AdapterMain()
-        binding.rvMain.layoutManager = LinearLayoutManager(this)
+        binding.rvMain.adapter = AdapterMain(this, listName)
+        binding.rvMain.layoutManager = GridLayoutManager(this, 2)
+
     }
 }
--- ExpressArc/app/src/main/java/com/example/expressarc/recycler/AdapterMain.kt
+++ ExpressArc/app/src/main/java/com/example/expressarc/recycler/AdapterMain.kt
@@ -1,27 +1,47 @@
 package com.example.expressarc.recycler
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.expressarc.databinding.ItemRecyclerBinding
+import com.example.expressarc.model.MainItem
 
-class AdapterMain(list: List<String>
-): RecyclerView.Adapter<ViewHolder>() {
+class AdapterMain(private val context: Context, private val list: List<MainItem>
+): RecyclerView.Adapter<AdapterMain.MainViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MainViewHolder {
         val binding = ItemRecyclerBinding.inflate(LayoutInflater.from(parent.context),parent, false)
         return MainViewHolder(binding)
     }
 
+    override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
+        val item = list[position]
+        holder.bind(item)
+    }
+
     override fun getItemCount(): Int {
-        return 10
+        return list.size
     }
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    inner class MainViewHolder(itemRecycler: ItemRecyclerBinding): ViewHolder(itemRecycler.root) {
 
-    }
+        private val txtNameButton: TextView
+        private val button = itemRecycler.btnClique
 
-    class MainViewHolder(itemRecycler: ItemRecyclerBinding): ViewHolder(itemRecycler.root) {
+        init {
+            txtNameButton = itemRecycler.txtNameButton
+        }
 
+        fun bind(mainItem: MainItem) {
+            txtNameButton.setText(mainItem.textStringId)
+            button.setOnClickListener {
+                Toast.makeText(context, "Clicou no button", Toast.LENGTH_SHORT).show()
+            }
+            button.setImageResource(mainItem.drawableId)
+        }
     }
 }
